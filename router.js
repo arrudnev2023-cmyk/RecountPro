fa2544a7ed98bd5c99e58f0ea06cc348f7c0dcaf
arrudnev2023-cmyk <ar.rudnev2023@gmail.com>
2026-02-24 23:07:05 +0300

    Create router.js

---
+++ router.js
@@ -0,0 +1,32 @@
+async function navigateTo(page) {
+    try {
+        const response = await fetch(`components/${page}.html`);
+        if (!response.ok) throw new Error("Страница не найдена");
+
+        const html = await response.text();
+        const app = document.getElementById("app");
+        app.innerHTML = html;
+
+        // Выполняем скрипты внутри компонента
+        const scripts = app.querySelectorAll("script");
+        scripts.forEach(oldScript => {
+            const newScript = document.createElement("script");
+
+            if (oldScript.src) {
+                newScript.src = oldScript.src;
+            } else {
+                newScript.textContent = oldScript.textContent;
+            }
+
+            document.body.appendChild(newScript);
+            oldScript.remove();
+        });
+
+    } catch (e) {
+        document.getElementById("app").innerHTML = "<p>Ошибка загрузки</p>";
+        console.error(e);
+    }
+}
+
+// При первом запуске — открываем Базу товаров
+navigateTo("Baza");
